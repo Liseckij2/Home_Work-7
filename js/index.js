968cf8a63e7fe3dfc835c929beabdc6b1d561d5c
Andrew Liseczkiy <45668739+Liseckij2@users.noreply.github.com>
2021-10-11 04:14:03 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,53 +1,54 @@
 function calculateNumber() {
-  do {
+    do {
     let repeatOperation;
     let sendNumber;
     do {
       sendNumber = prompt('Введите не менее 3-х чисел через пробел').split(' ');
     } while (sendNumber.length < 3);
     do {   
-    let sendOperation;
+      let sendOperation;
       sendOperation = +prompt(`Выберите операцию \n1- сортировка по возрастанию \n2- сортировка по убыванию \n3- четные не четные числа, сумма чисел, наибольшее и наеменьшее числа`);
-      let simbolForOperations;
+      let simbolForOperations = prompt('Выберите разделитель для этой операции   \' / \' или \' ; \'');
 
-      if (sendOperation === 1) {
-      simbolForOperations = prompt('Выберите разделитель для этой операции   \' / \' или \' ; \'  '); 
-           sendNumber = sendNumber.sort((a, b) => a - b).join(simbolForOperations);
-            alert(sendNumber);
+    if (sendOperation === 1) {
+      simbolForOperations 
+         sendNumber = sendNumber.sort((a, b) => a - b).join(simbolForOperations);
+         alert(sendNumber);
+         console.log(sendNumber);
     }
     if (sendOperation === 2) {
-      simbolForOperations = prompt('Выберите разделитель для этой операции   \' / \' или \' ; \'  ');
-      sendNumber = sendNumber.sort((a, b) => b - a).join(simbolForOperations);
-            alert(sendNumber);
+      simbolForOperations;
+        sendNumber = sendNumber.sort((a, b) => b - a).join(simbolForOperations);
+        alert(sendNumber);
+        console.log(sendNumber);
     }
     if (sendOperation === 3) {
       let resultSum = 0;
-      sendNumber.forEach(element => {
+        sendNumber.forEach(element => {
         resultSum += +element;
-         resultSum;
+        resultSum;
       });
       let resultLength = 0;  
-      sendNumber.forEach(element => {
+        sendNumber.forEach(element => {
         resultLength += (+element / sendNumber.length);
-         resultLength;
+        resultLength;
       });
       let finalResult = `\nСумма чисел ${resultSum} \nСреднее арифметическое ${resultLength}
       \nНаибольшее число ${Math.max.apply(Math.max, sendNumber)}\nНаименьшее число ${Math.min.apply(Math.min, sendNumber)}`;
-     
-      console.log(finalResult);
-       alert(finalResult);   
+         alert(finalResult);   
+         console.log(finalResult);
     }
     confirm('Повтор?');   
     repeatOperation = +prompt('Повторить с страрым массивом нажмите 1 \nС новым массивом нажмите 2');
     if (repeatOperation === 1); {
-      sendNumber;
-      sendOperation;
+        sendNumber;
+        sendOperation;
     }
   } while (repeatOperation === 1) 
-    if (repeatOperation === 2); {
-      calculateNumber();
-    }
-  }while(repeatOperation === 2);
+  if (repeatOperation === 2); {
+    calculateNumber();
+  }
+} while (repeatOperation === 2);
   };
   calculateNumber();
 
